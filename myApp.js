eadbfff2fe7da9c481a2933573480d52c8bb5352
Aly Khaled <alykhaled2001@live.com>
2021-07-24 10:14:32 +0200

    Challenge 4

--- myApp.js
+++ myApp.js
@@ -5,36 +5,9 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.use('/public',express.static(__dirname + '/public'));
 
 console.log("Hello World")
-
-
-
-
-
-
-
-
 
 
  module.exports = app;
