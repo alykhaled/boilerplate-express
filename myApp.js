7a0453ce282f39d0a39ccc72959e719d03b0f528
Aly Khaled <alykhaled2001@live.com>
2021-07-24 10:05:20 +0200

    Challenge 3

--- myApp.js
+++ myApp.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
 });
 
 
