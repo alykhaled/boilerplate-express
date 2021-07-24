fa2d99e6e9b72fe44c7955f3d312d6498066bc7c
Aly Khaled <alykhaled2001@live.com>
2021-07-24 12:08:06 +0200

    Challenge 5

--- myApp.js
+++ myApp.js
@@ -7,6 +7,12 @@ app.get('/', (req, res) => {
 
 app.use('/public',express.static(__dirname + '/public'));
 
+
+app.get('/json', (req, res) => {
+    res.json({"message": "Hello json"});
+  });
+  
+
 console.log("Hello World")
 
 
